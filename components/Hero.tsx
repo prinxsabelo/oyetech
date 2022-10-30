0cbcda2c84c3776982cb7dbf47591cd040598d92
Bello Samuel <prinxsabelo@gmail.com>
2022-10-30 12:16:59 +0100

    ..

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import ButtonLink from './ButtonLink';
 const Hero = () => {
   return (
     <div className='mt-28 lg:mt-30 flex flex-col lg:flex-row items-center space-y-8 md:space-y-0'>
-        <div className='w-full lg:w-1/2 flex flex-col space-y-4 lg:space-y-8'>
+        <div className='w-full lg:w-1/2 flex flex-col space-y-6 lg:space-y-8'>
             <div className="testimonial-item text-start">
                 <h1 className="text-4xl lg:text-6xl mb-2 mt-2 font-bold leading-tight 
                     " >
